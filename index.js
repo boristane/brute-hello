2207b774fd405d4d48eeb79775798ff10ffdf6d4
Boris Tane <boris.tane@gmail.com>
2018-05-04 20:18:21 +0100

    completes brute force function

---
+++ index.js
@@ -0,0 +1,24 @@
+function bruteHello(){
+
+    const str = 'Hello, world!';
+    const arr = str.split('');
+
+    let result = '';
+    let i = 0;
+
+    let interval = setInterval(() => {
+        const char = arr[i];
+        const randCharCode = Math.floor(Math.random()*94) + 32;
+        const c = String.fromCharCode(randCharCode);
+        console.log(result + c);
+        if(c === char){
+            result += char;
+            i++;
+        }
+        if(result === str){
+            clearInterval(interval);
+        }
+    }, 10);
+}
+
+
